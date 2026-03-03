f6282a399fc22dad581878fb4a61e1a1411f4e77
Rehman <rehman.ahmad2@metropolia.fi>
2026-03-03 12:27:29 +0200

    Lentopeli projekti.

--- projekti/Peli.py
+++ projekti/Peli.py
@@ -1,1 +1,15 @@
+#Yees löysit Baarista haalarimerkki.
+#Minä teen koodi ilmoitusviesti esim teen sellaista kun löydän haalarimerkkin
+#  niin siitä tule ilmoitus ja teen funktion joka pitää kirjaa
+#montako haalarimerkkiä on löydetty
+
+def haalarimerkki():
+    print("Haalarimerkki löydetty!")
+
+
+def haalarimerkki_määrä(maara):
+    print("Haalarimerkkiä on löydetty:", maara)
+
+
+
 
