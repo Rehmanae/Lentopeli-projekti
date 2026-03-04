3ff333cf105d6d45576e6c13a7e90815b52da793
Rehman <rehman.ahmad2@metropolia.fi>
2026-03-04 11:11:25 +0200

    Lentopeli projekti

--- projekti/Oma koodi.py
+++ projekti/Oma koodi.py
@@ -1,3 +1,5 @@
+"""
+
 #Yees löysit Baarista haalarimerkki.
 #Minä teen koodi ilmoitusviesti esim teen sellaista kun löydän haalarimerkkin
 #  niin siitä tule ilmoitus ja teen funktion joka pitää kirjaa
@@ -16,14 +18,26 @@ def haalarimerkki_määrä():
     maara = input("Kuinka paljon Haalarimerkki on löydetty?:")
     return haalarimerkki
 
-# haalarimerkki = input("Montako haalarimerkki on nyt?:")
+haalarimerkki = input("Montako haalarimerkki on nyt?:")
+haalarimerkki.append("maara")
 
-while haalarimerkki >=1:
-    haalarimerkki.append("maara")
-
-#while True:
-#    print("Haalarimerkkiä on löydetty:", maara)
+while True:
+    print("Haalarimerkkiä on löydetty:", maara)
 
 
+
+"""
+# Kirjoita funktio / (koodi) joka laskee etäisyyden appropiste1
+#ja appropiste 2 välillä, esim. piste 1k - piste 2k = väli/matka on 1k
+
+
+import random
+
+def appropiste1():
+    2000-1000
+
+
+
+appropiste2 =
 
 
---
+++ projekti/UUsi koodi.py
@@ -0,0 +1,11 @@
+# Kirjoita funktio / (koodi) joka laskee etäisyyden appropiste1
+#ja appropiste 2 välillä, esim. piste 1k - piste 2k = väli/matka on 1k
+
+
+import random
+
+def appropiste1():
+    2000-1000
+
+
+appropiste2 = matka
