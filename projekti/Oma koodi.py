6dc142c2dc6a7294b8395a7e8fc05e4d012298f0
Rehman <rehman.ahmad2@metropolia.fi>
2026-03-05 15:57:51 +0200

    Lentopeli projekti

--- projekti/Oma koodi.py
+++ projekti/Oma koodi.py
@@ -2,15 +2,19 @@
 #Teen koodi, ilmoitusviesti esim kun pelaaja löytää haalarimerkkin
 #Niin hänelle tule ilmoitus siitä ja teen funktion joka pitää kirjaa montako haalarimerkkiä on löydetty yhteensä.
 
+
+# Tässä FUnktio lisää yhden haalarimerkkin määrän
+# Sen jälkeen lisää yhden haalrimerkkin ja näyttää uuden määrän
 def haalarimerkki(maara):
     maara = maara +1
     print("Haalarimerkki löydetty!")
     print("Sinulla on nyt", maara, "kpl")
     return maara
 
-#Kysytään käyttäjältä kuinka monta haalarimerkki on löydetty?
+#Kysytään pelaajalta kuinka monta haalarimerkki hänellä on?
 maara = int(input("Montako haalarimerkkiä sinulla on?: "))
 
+#Kutsutaan funktiota ja päivitetään määrä.
 maara = haalarimerkki(maara)
 
 
