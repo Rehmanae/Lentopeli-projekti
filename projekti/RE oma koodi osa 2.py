cd0ab1aec7152699910ff4cbf0910bfabd2b1958
Rehman <rehman.ahmad2@metropolia.fi>
2026-03-09 09:44:43 +0200

    Lentopeli projekti

--- projekti/RE oma koodi osa 2.py
+++ projekti/RE oma koodi osa 2.py
@@ -1,13 +1,12 @@
-# Kirjoita funktio / (koodi) joka laskee etäisyyden appropiste1
-#ja appropiste 2 välillä, esim. piste 1k - piste 2k = väli/matka on 1k
+# Kirjoitan funktio tai koodin, joka laskee etäisyyden appropiste1
+#ja appropiste 2 väliltä, esim. piste 1k - piste 2k = väli/matka on 1k
 
 
 import random
 
 # Tässä Funktio laskee kahden pisteen välisen etäisyyden
 def appropiste(piste1, piste2):
-    # Tarkistaa kumpi pisteistä on suurempi
-    if piste1 > piste2:
+    if piste1 > piste2:              # Tarkistaa kumpi pisteistä on suurempi
         erotus = piste2 - piste1     # Laskee pisteiden välinen erotus
     else:
         erotus = piste2 - piste1     # Eli jos piste2 on suurempi niin lasketaan näin
