9ca922e18011ce6cb23db1676df127d127b04287
Rehman <rehman.ahmad2@metropolia.fi>
2026-03-09 09:26:11 +0200

    Lentopeli projekti

--- projekti/RE oma koodi osa 2.py
+++ projekti/RE oma koodi osa 2.py
@@ -11,11 +11,15 @@ def appropiste(piste1, piste2):
         erotus = piste2 - piste1     # Laskee pisteiden välinen erotus
     else:
         erotus = piste2 - piste1     # Eli jos piste2 on suurempi niin lasketaan näin
-    return erotus
+    return erotus   # Palauttaa lasketun etäisyyden
 
+
+#Arvotaan eka piste väliltä 1000-10000
 appropiste1 = random.randint(1000,10000)
 
+#Arvotaan toka piste väliltä 1000-1000
 appropiste2 = random.randint(1000,10000)
+
 
 print(appropiste1)
 print(appropiste2)
