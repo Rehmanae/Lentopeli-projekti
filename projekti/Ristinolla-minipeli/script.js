cc47891492a480dd04aaf6fe5b8aa3b5de9a2d9f
Rehman <rehman.ahmad2@metropolia.fi>
2026-05-02 00:52:48 +0300

    Lisätään JavaScript alku ja pelin muuttujat.

---
+++ projekti/Ristinolla-minipeli/script.js
@@ -0,0 +1,8 @@
+console.log("Ristinolla peli käynnistyi")
+
+// Pelaaja on X ja botti on O.
+let pelaaja = "X"
+let peliOhi = false
+
+let aani = new Audio("ting.mp3")
+let voittoAani = new Audio("gameover.mp3")
