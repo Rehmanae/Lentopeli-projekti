229210f08641bb987547f3d667f0a992559ba955
Rehman <rehman.ahmad2@metropolia.fi>
2026-05-03 20:40:36 +0300

    Lisäsin muuttujat pyöritystä varten ja pyorita funktion rungon.

--- projekti/Roulette-minipeli/static/script.js
+++ projekti/Roulette-minipeli/static/script.js
@@ -2,3 +2,14 @@ let wheel = document.getElementById("wheel");
 let tulos = document.getElementById("tulos");
 
 let vaihtoehdot = ["Voitit", "Hävisit", "Uudestaan", "Merkki"];
+
+let pyorii = false;
+let asteet = 0;
+
+function pyorita() {
+    if (pyorii) {
+        return;
+    }
+
+    pyorii = true;
+}
